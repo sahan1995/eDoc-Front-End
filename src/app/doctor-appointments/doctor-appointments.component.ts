9b9cb9069049fecd7c2ecadee0d1899c6a1b19bd
sahan1995 <sahanrajakaruna001@gmail.com>
2019-02-21 16:30:29 +0530

    get doctor appointments

--- src/app/doctor-appointments/doctor-appointments.component.ts
+++ src/app/doctor-appointments/doctor-appointments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {DoctorAppointmentsService} from "../service/doctor-appointments.service";
 
 @Component({
   selector: 'app-doctor-appointments',
@@ -8,17 +9,66 @@ import {Router} from "@angular/router";
 })
 export class DoctorAppointmentsComponent implements OnInit {
 
-  constructor(private route:Router) { }
+  constructor(private route:Router,private doctorAppSer:DoctorAppointmentsService) { }
   private user_Full_Name;
+  private DID;
+  private appointments;
+  private appointDetails :any;
+  private appType = "Type of Appointment"
   ngOnInit() {
 
     if(localStorage.getItem("fname")==null){
       this.route.navigate(["/SignIn"])
-    }else{
-      this.user_Full_Name = localStorage.getItem("fname")+" "+localStorage.getItem("lname");
-
+      return;
     }
+      this.user_Full_Name = localStorage.getItem("fname")+" "+localStorage.getItem("lname");
+      this.DID = localStorage.getItem("id");
 
+      this.getDoctorAppointments(this.DID);
+
+  }
+
+  getDoctorAppointments(DID){
+
+    this.doctorAppSer.getDoctorAppointments(DID).subscribe(result=>{
+      this.appointments = result;
+
+      this.appointments.forEach(appoint=>{
+        // this.appointDetails["appCode"] = appoint.patientDTO.fname;
+        // console.log(appoint.patientDTO.fname)
+        this.appointDetails = appoint;
+        appoint["patientName"] = appoint.patientDTO.fname+" "+appoint.patientDTO.lname;
+        appoint["time"]=this.tConvert(appoint.time);
+        this.doctorAppSer.getProPic(appoint.patientDTO.profilePic).subscribe(result=>{
+
+          let reader= new FileReader();
+          reader.addEventListener("load",()=>{
+            appoint["patientPic"]=reader.result;
+          },false)
+          if(result){
+            const  img = result as Blob
+            reader.readAsDataURL(img)
+          }
+
+        })
+
+      })
+
+
+    })
+
+  }
+
+   tConvert (time) {
+    // Check correct time format and split into components
+    time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+    if (time.length > 1) { // If time format correct
+      time = time.slice (1);  // Remove full string match value
+      time[5] = +time[0] < 12 ? ' AM ' : ' PM' ; // Set AM/PM
+      time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    return time.join (''); // return adjusted time or original string
   }
 
 }
---
+++ src/app/service/doctor-appointments.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorAppointmentsService {
+
+  constructor(private http:HttpClient) { }
+
+
+  private url= "http://localhost:8080/api/v1/"
+  getDoctorAppointments(DID){
+
+    return this.http.get(this.url+"doctors/getDoctorAppointments?DID="+DID);
+
+  }
+
+  getProPic(picName){
+    const httpOptions = {
+      'responseType'  : 'blob' as 'json'
+      //'responseType'  : 'blob' as 'json'        //This also worked
+    };
+    return this.http.get(this.url+"patients/getPic?picName="+picName,httpOptions)
+  }
+}
