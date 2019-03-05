952f5faff745278a7e6b8d36827670c325f96d7a
sahan1995 <sahanrajakaruna001@gmail.com>
2019-03-05 23:40:57 +0530

    video consultation fixed

--- src/app/doctor-videochat-dashboard/doctor-videochat-dashboard.component.ts
+++ src/app/doctor-videochat-dashboard/doctor-videochat-dashboard.component.ts
@@ -51,7 +51,7 @@ export class DoctorVideochatDashboardComponent implements OnInit {
 
 
   ngOnInit() {
-  console.log(this.morning)
+
 
     this.arry = Array(this.count).fill(0).map((x,i)=>i);
     if (localStorage.getItem("fname") == null) {
@@ -87,22 +87,37 @@ export class DoctorVideochatDashboardComponent implements OnInit {
     // })
 
     //--------------------------------------------------------------------------
-    //Answer the request come from other end
+    // Answer the request come from other end
     var n = <any>navigator;
     n.getUserMedia = n.getUserMedia||n.webkitGetUserMedia||n.mozGetUserMedia
     this.peer.on('call',function (call) {
       n.getUserMedia({video:true,audio:true},function (stream) {
-        call.answer(stream)
-        call.on('stream',function (remotestream) {
-          video.src = URL.createObjectURL(remotestream);
-          video.play()
-        })
+
+       if(stream.active==true){
+
+         // if(confirm("Patient is Calling......")){
+           call.answer(stream)
+           call.on('stream',function (remotestream) {
+             video.src = URL.createObjectURL(remotestream);
+             video.play()
+           })
+         // }
+
+         // if (confirm("Patient Calling.........................")) {
+
+         // } else {
+         //      return;
+         // }
+
+       }
+
+
       },function (err) {
         console.log('Failed to get local stream' ,err);
       })
     })
     //-----------------------------------------------------------------------------
-    this.getPatientKey();
+
   }
 
   // connect() {
@@ -114,30 +129,31 @@ export class DoctorVideochatDashboardComponent implements OnInit {
 
   //--------------------------------------------------------------------------
   //Connect to the the other id and video call
-  videoConnect(key) {
-    let video = this.myVideo.nativeElement;
-    var localvar = this.peer;
-    var fname =key;
-    console.log(fname)
-    var n = <any>navigator;
-    n.getUserMedia = n.getUserMedia||n.webkitGetUserMedia||n.mozGetUserMedia
-
-    n.getUserMedia({video:true,audio:true},function (stream) {
-      var call = localvar.call(fname,stream);
-      call.on('stream',function (remotestream) {
-        video.src = URL.createObjectURL(remotestream)
-        video.play();
-      })
-    },function (err) {
-      console.log('Failed to get local stream' ,err);
-    })
+  // videoConnect(key) {
+  //   let video = this.myVideo.nativeElement;
+  //   var localvar = this.peer;
+  //   var fname =key;
+  //   console.log(fname)
+  //   var n = <any>navigator;
+  //   n.getUserMedia = n.getUserMedia||n.webkitGetUserMedia||n.mozGetUserMedia
+  //
+  //   n.getUserMedia({video:true,audio:true},function (stream) {
+  //     var call = localvar.call(fname,stream);
+  //     call.on('stream',function (remotestream) {
+  //       video.src = URL.createObjectURL(remotestream)
+  //       video.play();
+  //     })
+  //   },function (err) {
+  //     console.log('Failed to get local stream' ,err);
+  //   })
   //-------------------------------------------------------------------------------
-  }
+  // }
 
 
   getPatientKey(){
     this.doctorChatService.getPatientKey(this.PID).subscribe(result=>{
-      this.videoConnect(result)
+     this.patientKey = result;
+     console.log(result)
     })
   }
   updateKey(DID,key){
@@ -207,11 +223,23 @@ export class DoctorVideochatDashboardComponent implements OnInit {
   }
 e
   finishAppointment(){
+
+    var thisCom  = this;
+    this.doctorChatService.getPatientKey(this.PID).subscribe(result=>{
+      this.patientKey = result;
       console.log(this.patientKey)
-    var conn = this.peer.connect(this.patientKey);
-    conn.on('open', function () {
-      conn.send("hello")
+      var conn = this.peer.connect(result);
+      conn.on('open', function () {
+        conn.send(true)
+        thisCom.doctorChatService.finishAppointment(thisCom.appCode).subscribe(done=>{
+          console.log(done)
+          console.log(thisCom.peer)
+          thisCom.peer.disconnect();
+        })
+      })
     })
+
+
     // this.route.navigate(["Doctor-Appointment"])
 
     // this.route.navigateByUrl('/Doctor-VideoChat-DashBoard', {skipLocationChange: true}).then(()=>
--- src/app/patient-videochat-dashboard/patient-videochat-dashboard.component.ts
+++ src/app/patient-videochat-dashboard/patient-videochat-dashboard.component.ts
@@ -1,15 +1,20 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewChecked, Component, OnInit, ViewChild} from '@angular/core';
 import {VideoChatService} from "../service/video-chat.service";
 import {DoctorChatService} from "../service/doctor-chat.service";
 import {PatientVideochatService} from "../service/patient-videochat.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Promise} from "q";
+
 declare var Peer: any;
+declare let paypal:any;
 @Component({
   selector: 'app-patient-videochat-dashboard',
   templateUrl: './patient-videochat-dashboard.component.html',
   styleUrls: ['./patient-videochat-dashboard.component.css']
 })
-export class PatientVideochatDashboardComponent implements OnInit {
+
+
+export class PatientVideochatDashboardComponent implements OnInit,AfterViewChecked {
 
   @ViewChild("myVideo")
   myVideo: any;
@@ -22,10 +27,39 @@ export class PatientVideochatDashboardComponent implements OnInit {
   private DID;
   private appCode
   private callConnected = false;
-  private drugs :any;
+  private drugs: any;
   private showPres = false;
-  constructor(private videoChat:VideoChatService, private patientVideoChatSer:PatientVideochatService,
-              private route:Router,private Aroute: ActivatedRoute,) {
+  private bootbox = this;
+
+  private addScpipt : boolean = false;
+  private finalAmount : number = 1;
+
+
+  private paypalConfig = {
+    env : 'sandbox',
+    client:{
+      sandbox : 'AacN8iysR1yat5jumlfFPF05UVLPfYPuXL3XTG3WknnjLa1P_xFWjwmsjHwtiDBpymRZ_3xHM9cGgX16',
+
+    },
+    commit : true,
+    payment:(data,actions)=>{
+      return actions.payment.create({
+        payment:{
+          transactions:[
+            {amount:{total:this.finalAmount,currency:'USD'}}
+          ]
+        }
+      });
+    },
+    onAuthorize : (data,actions)=>{
+      return actions.payment.execute().then((payment)=>{
+        console.log("Success ")
+      })
+    }
+  };
+
+  constructor(private videoChat: VideoChatService, private patientVideoChatSer: PatientVideochatService,
+              private route: Router, private Aroute: ActivatedRoute,) {
   }
 
 
@@ -41,7 +75,7 @@ export class PatientVideochatDashboardComponent implements OnInit {
     this.appCode = this.Aroute.snapshot.params.appCode;
     console.log(this.appCode)
 
-    console.log(this.DID+" "+this.appCode)
+    console.log(this.DID + " " + this.appCode)
 
     // let video = this.myVideo.nativeElement;
     this.peer = new Peer({key: "p17fpt3b2vnuq5mi"})
@@ -50,26 +84,44 @@ export class PatientVideochatDashboardComponent implements OnInit {
       this.setMyKey(this.myPeerID)
       this.updateMyKey(this.myPeerID);
     }, 1000)
+
+
+    var thisCom = this;
+    // var test = this.endCallAndShowPrescription();
     this.peer.on('connection', function (conn) {
       conn.on('data', function (data) {
-      console.log(data)
+        if (data) {
+          // thisCom.endCallAndShowPrescription();
+          console.log()
+
+          thisCom.patientVideoChatSer.getPrescription(thisCom.appCode).subscribe(result => {
+            console.log(result.drugs);
+            thisCom.showPres = true;
+            thisCom.drugs = result.drugs;
+
+          })
+        }
       })
     })
+
     var n = <any>navigator;
-    n.getUserMedia = n.getUserMedia||n.webkitGetUserMedia||n.mozGetUserMedia
-    this.peer.on('call',function (call) {
-      n.getUserMedia({video:true,audio:true},function (stream) {
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia
+    this.peer.on('call', function (call) {
+      n.getUserMedia({video: true, audio: true}, function (stream) {
         call.answer(stream)
-        call.on('stream',function (remotestream) {
+        call.on('stream', function (remotestream) {
           video.src = URL.createObjectURL(remotestream);
           video.play()
+
         })
-      },function (err) {
-        console.log('Failed to get local stream' ,err);
+      }, function (err) {
+        console.log('Failed to get local stream', err);
       })
     })
 
     this.getKey();
+
+
   }
 
   connect() {
@@ -80,10 +132,10 @@ export class PatientVideochatDashboardComponent implements OnInit {
   }
 
 
-  getDoctorKey(){
+  getDoctorKey() {
 
-    this.patientVideoChatSer.getDoctorKey(this.DID).subscribe(result=>{
-        this.videoConnect(result)
+    this.patientVideoChatSer.getDoctorKey(this.DID).subscribe(result => {
+      this.videoConnect(result)
     })
 
   }
@@ -97,43 +149,71 @@ export class PatientVideochatDashboardComponent implements OnInit {
     // var fname = this.anotherid;
 
     var n = <any>navigator;
-    n.getUserMedia = n.getUserMedia||n.webkitGetUserMedia||n.mozGetUserMedia
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia
 
-    n.getUserMedia({video:true,audio:true},function (stream) {
-      var call = localvar.call(key,stream);
-      call.on('stream',function (remotestream) {
+    n.getUserMedia({video: true, audio: true}, function (stream) {
+      var call = localvar.call(key, stream);
+
+      console.log(call)
+
+      call.on('stream', function (remotestream) {
 
         video.src = URL.createObjectURL(remotestream)
         video.play();
       })
-    },function (err) {
-      console.log('Failed to get local stream' ,err);
+
+
+    }, function (err) {
+      console.log('Failed to get local stream', err);
     })
 
   }
 
-    getKey(){
-    this.videoChat.cast.subscribe(result=>{
+  getKey() {
+    this.videoChat.cast.subscribe(result => {
       this.doctorKey = result;
     })
-    }
+  }
 
-    setMyKey(key){
+  setMyKey(key) {
 
     this.videoChat.addKey(key);
-    }
+  }
 
-    updateMyKey(key){
-      this.patientVideoChatSer.updateKey(this.PID,key).subscribe(result=>{})
-    }
+  updateMyKey(key) {
+    this.patientVideoChatSer.updateKey(this.PID, key).subscribe(result => {
+    })
+  }
 
-    endCallAndShowPrescription(){
+  endCallAndShowPrescription() {
 
     this.showPres = true;
-    this.patientVideoChatSer.getPrescription(this.appCode).subscribe(result=>{
+    this.patientVideoChatSer.getPrescription(this.appCode).subscribe(result => {
       this.drugs = result.drugs;
     })
 
+  }
+
+
+  addPaypalScript(){
+    this.addScpipt = true;
+    return new Promise((resolve,reject)=>{
+
+      let scripttagElement = document.createElement('script')
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement)
+
+    })
+  }
+
+  ngAfterViewChecked():void{
+    if(!this.addScpipt){
+      this.addPaypalScript().then(()=>{
+
+        paypal.Button.render(this.paypalConfig,'paypal-checkout-btn')
+      })
     }
+  }
 
 }
